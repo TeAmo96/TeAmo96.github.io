67895daaba87cbe31be7a2d80fcd6e8ac201ad53
TeAmo96 <felicidadesteamomucho@gmail.com>
2024-08-10 05:58:36 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+function logueo(event) {
+    event.preventDefault(); // Previene el comportamiento predeterminado del formulario
+
+    const correctPassword = "12345"; // Cambia esta contraseña por la que necesites
+    const inputPassword = document.getElementById("contra").value;
+    const errorMessage = document.getElementById("error-message");
+
+    if (inputPassword === correctPassword) {
+        localStorage.setItem("loggedIn", "true");
+        window.location.href = "welcome.html"; // Redirecciona a la página secreta
+    } else {
+        errorMessage.style.display = "block"; // Muestra el mensaje de error
+    }
+
+    return false; // Retorna false para asegurar que el formulario no se envíe
+}
